76fe4ae5a9b97c4353d7c99822d88d9364ace09c
Liliana Matos <liliana@1stdibs.com>
2022-01-26 17:11:35 -0500

    Implement support for @defer directive

--- src/index.ts
+++ src/index.ts
@@ -329,6 +329,9 @@ export type {
   ExecutionArgs,
   ExecutionResult,
   FormattedExecutionResult,
+  ExecutionPatchResult,
+  FormattedExecutionPatchResult,
+  AsyncExecutionResult,
 } from './execution/index';
 
 export type { SubscriptionArgs } from './subscription/index';
